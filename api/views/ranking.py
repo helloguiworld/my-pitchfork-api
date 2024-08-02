b616b2b7f1190804207473fed58a08c8ecb952dc
Guilherme Feitosa <feitosa.guilhermef@gmail.com>
2024-08-02 18:01:03 -0300

    ranking year

--- api/views/ranking.py
+++ api/views/ranking.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.utils import timezone
+from dateutil.relativedelta import relativedelta
 from django.db.models import Count, Avg, Sum
 from common.permissions import IsMyOriginOrAdmin
 from spotify.models import Album
@@ -31,6 +32,29 @@ class AlbumRankingViewSet(viewsets.ViewSet):
         } for position, album in enumerate(new_releases_albums, start=1)]
         return Response(new_releases_ranking)
 
+    @action(detail=False, methods=['get'])
+    def year(self, request):
+        # RELEASES OF THE YEAR (1 month = 4 weeks, max 20)
+        max_new_releases_ranking = 20
+        one_year_ago = timezone.now() - relativedelta(years=1)
+        one_year_ago_str = one_year_ago.date().isoformat()
+        new_releases_albums = (
+            Album.objects
+                .filter(data__date__gte=one_year_ago_str)
+                .annotate(reviews_count=Count('reviews'))
+                .filter(reviews_count__gt=0)
+                .annotate(reviews_score_avg=Avg('reviews__score'), reviews_score_sum=Sum('reviews__score'))
+                .order_by('-reviews_score_sum', '-reviews_count', '-data__date', 'data__name')[:max_new_releases_ranking]
+        )
+        new_releases_ranking = [{
+            "position": position,
+            "album": album.data,
+            "reviews_count": album.reviews_count,
+            "reviews_sum": album.reviews_score_sum,
+            "reviews_avg": round(album.reviews_score_avg, 1),
+        } for position, album in enumerate(new_releases_albums, start=1)]
+        return Response(new_releases_ranking)
+    
     @action(detail=False, methods=['get'])
     def all(self, request):
         # ALL ALBUMS (max 20)
